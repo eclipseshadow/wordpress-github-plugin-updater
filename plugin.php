740a9ae85c06e2883d94a9e864cb890f962967ff
Zach <zach@zachlanich.com>
2013-08-12 04:19:24 -0400

    Initial Commit

---
+++ plugin.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+Plugin Name: GitHub Plugin Updater
+Plugin URI:
+Description: Allows automatic updating for plugins hosted on GitHub
+Version: 0.1
+Author: Zach Lanich
+Author URI: https://www.ZachLanich.com
+License: Undecided
+*/
+
+/**
+ * Class WP_GitHub_Plugin_Updater
+ *
+ * Allows automatic updating for plugins hosted on GitHub
+ */
+class WP_GitHub_Plugin_Updater {
+
+	public function __construct() {
+
+		require_once 'lib/updater.php';
+
+		if ( is_admin() ) {
+
+			$config = array(
+				// These top 2 might not work on older PHP installs - I've seen basename() behave strangely
+				'slug' => basename(dirname(__FILE__)) .'/'. basename(__FILE__),
+				'proper_folder_name' => basename(dirname(__FILE__)),
+				'api_url' => 'https://api.github.com/repos/eclipseshadow/wordpress-github-plugin-updater',
+				'raw_url' => 'https://raw.github.com/eclipseshadow/wordpress-github-plugin-updater/master',
+				'github_url' => 'https://github.com/eclipseshadow/wordpress-github-plugin-updater',
+				'zip_url' => 'https://github.com/eclipseshadow/wordpress-github-plugin-updater/archive/master.zip',
+				'sslverify' => true,
+				'requires' => '3.0',
+				'tested' => '3.6',
+				'readme' => 'README.md',
+				'access_token' => '',
+			);
+
+			new WP_GitHub_Updater( $config );
+
+		}
+
+	}
+
+}
+
+add_action('init', create_function('', '$wp_github_plugin_updater = new WP_GitHub_Plugin_Updater();'));
